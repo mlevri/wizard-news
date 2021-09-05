ceb18de00b89c1d8aa08509f9a5d3987b935f425
Mandy Levri <mandylevri@gmail.com>
2021-09-05 11:47:57 -0400

    final project

--- app.js
+++ app.js
@@ -1,9 +1,76 @@
 const express = require("express");
+const morgan = require('morgan');
+const postBank = require("./postBank")
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
 const PORT = 1337;
+
+app.use(morgan('dev'));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span><a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+  ).join('')}
+    </div>
+  </body>
+</html>`;
+  res.send(html)
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  if (!post.id) {
+    next(err)
+  }else{
+  const singlePostHTML = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-info'>
+          <p>
+            ${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <p>
+            ${post.content}
+          </p>
+        </div>
+  </body>
+</html>`;
+  res.send(singlePostHTML);
+}});
+
+app.use(function (err, req, res, next) {
+  res.status(404);
+  res.send("<h1>404 Error, Page Not Found!</h1>")
+})
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
